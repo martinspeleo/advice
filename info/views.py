0852606ae58915aee745feb5710d7cf1f52c357f
Martin Green <martin.speleo@gmail.com>
2014-05-26 15:56:18 +0100

    Display of sections in form and boiler plate for extra fields

--- info/views.py
+++ info/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render_to_response
 from models import *
 
 def home(request):
-    return render_to_response("index.html", {"leaflets": Leaflet.objects.all()})
+    return render_to_response("index.html", 
+                              {"leaflets": Leaflet.objects.all()})
 
 def leaflet(request, leaflet_pk):
-    return render_to_response("leaflet.html", {"leaflet": Leaflet.objects.get(pk = leaflet_pk)})
+    return render_to_response("leaflet.html", 
+                              {"leaflet": Leaflet.objects.get(pk = leaflet_pk),
+                               "extra_forms": []})
