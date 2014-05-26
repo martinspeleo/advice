fd74fbdd23b8395c2ee66b2001668a86364c0379
Martin Green <martin.speleo@gmail.com>
2014-05-26 15:42:55 +0100

    Home template and leaflet view

--- info/views.py
+++ info/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
+from models import *
 
 def home(request):
-    return render_to_response("index.html")
+    return render_to_response("index.html", {"leaflets": Leaflet.objects.all()})
 
 def leaflet(request, leaflet_pk):
-    return render_to_response("leaflet.html")
+    return render_to_response("leaflet.html", {"leaflet": Leaflet.objects.get(pk = leaflet_pk)})
