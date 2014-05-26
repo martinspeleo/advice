399e645bb541a30f8e0acb8e036d7420eceff0a5
Martin Green <martin.speleo@gmail.com>
2014-05-26 16:29:40 +0100

    Another small step to making a leaflet form

--- info/views.py
+++ info/views.py
@@ -6,6 +6,8 @@ def home(request):
                               {"leaflets": Leaflet.objects.all()})
 
 def leaflet(request, leaflet_pk):
+    extra_forms = []
+    leaflet = Leaflet.objects.get(pk = leaflet_pk)
     return render_to_response("leaflet.html", 
-                              {"leaflet": Leaflet.objects.get(pk = leaflet_pk),
-                               "extra_forms": []})
+                              {"leaflet": leaflet,
+                               "extra_forms": extra_forms})
