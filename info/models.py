7ecfc34407aa88f4af280564ef131c72747a285a
Martin Green <martin.speleo@gmail.com>
2014-05-28 19:56:17 +0100

    Added ordering of leaflets, sections and items.

--- info/models.py
+++ info/models.py
@@ -6,26 +6,38 @@ class Leaflet(models.Model):
     url_QR = models.BooleanField()
     long_QR = models.BooleanField()
     text = models.BooleanField()
+    order = models.IntegerField()
 
     def __unicode__(self):
         return unicode(self.title)
+
+    class Meta:
+        ordering = ['order']
 
 class Section(models.Model):
     leaflet = models.ForeignKey("info.Leaflet")
     title = models.CharField(max_length=40)
     unique =  models.BooleanField()
     required = models.BooleanField()
+    order = models.IntegerField()
 
     def __unicode__(self):
         return unicode(self.title)
+
+    class Meta:
+        ordering = ['order']
 
 class Item(models.Model):
     section = models.ForeignKey("info.Section")
     title = models.CharField(max_length=100)
     content = models.TextField()
+    order = models.IntegerField()
 
     def __unicode__(self):
         return unicode(self.title)
+
+    class Meta:
+        ordering = ['order']
 
 class Custom(models.Model):
     label = models.CharField(unique=True, max_length=50)
