614ca48eaee4601628f6be4fc491a203d8400fc2
Felicia <femgreen@gmail.com>
2014-06-08 17:21:16 +0100

    Error messages styled, header squished, changes to form, and beginning styling admin.

--- info/admin.py
+++ info/admin.py
@@ -1,7 +1,22 @@
 from info.models import *
 from django.contrib import admin
 
-admin.site.register(Leaflet)
-admin.site.register(Section)
-admin.site.register(Item)
+class SectionInline(admin.TabularInline):
+    model = Section
+    extra = 3
+
+class LeafletAdmin(admin.ModelAdmin):
+    list_display = ('title', 'email', 'url_QR', 'long_QR', 'text', 'order')
+    inlines = [SectionInline]
+
+class ItemInline(admin.StackedInline):
+    model = Item
+    extra = 3
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('leaflet', 'title', 'unique', 'required', 'order') 
+    inlines = [ItemInline]
+
+admin.site.register(Leaflet, LeafletAdmin)
+admin.site.register(Section, SectionAdmin)
 admin.site.register(Custom)
